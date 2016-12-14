f6d28f500f2f67d7184646b8896b0f9bf9951199
aleener <alexleener@gmail.com>
2016-12-14 16:26:52 +0200

    ft fnctn that dspls all diff combs of thre diff dgts

--- ft_print_comb/ft_print_comb.c
+++ ft_print_comb/ft_print_comb.c
@@ -6,7 +6,7 @@
 /*   By: aleener <alexleener@gmail.com>             ______ | || |_ __) |      */
 /*                                                 |______||__   _/ __/       */
 /*   Created: 2016/12/14 12:55:21 by aleener                  |_||_____|      */
-/*   Updated: 2016/12/14 15:27:44 by aleener      UNIT Factory                */
+/*   Updated: 2016/12/14 16:24:33 by aleener      UNIT Factory                */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,23 +19,23 @@ void	ft_putchar(char c)
 
 void	ft_print_comb(void)
 {
-	char first;
-	char second;
-	char third;
+	int first;
+	int second;
+	int third;
 
-	first = 47;
-	while (first++ <= '7')
+	first = -1;
+	while (++first <= 7)
 	{
 		second = first;
-		while (++second <= '8')
+		while (++second <= 8)
 		{
 			third = second;
-			while (++third <= '9')
+			while (++third <= 9)
 			{
-				ft_putchar(first);
-				ft_putchar(second);
-				ft_putchar(third);
-				if (first != '7')
+				ft_putchar(first + '0');
+				ft_putchar(second + '0');
+				ft_putchar(third + '0');
+				if (first != 7)
 				{
 					ft_putchar(',');
 					ft_putchar(' ');
@@ -48,6 +48,5 @@ void	ft_print_comb(void)
 int		main(void)
 {
 	ft_print_comb();
-	ft_putchar('\n');
 	return (0);
 }
